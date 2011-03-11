d815e1d47db3cd70df40ca8fb19085e57a60430f
Tony Milne <t.milne.09@aberdeen.ac.uk>
2011-03-11 01:29:19 +0000

    removed pending spec tests from helpers; added site spec test

--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Site do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "given that sites exist" do
+    before(:each) do
+      Bookmark.create!(:url => "http://www.alphasights.com/contact")
+      Bookmark.create!(:url => "http://www.bridgenoble.com/")
+    end
+
+    it "should be searchable" do
+      Site.search("alphasights").should == [] << Site.find_by_url("alphasights.com")
+    end
+  end
 end
